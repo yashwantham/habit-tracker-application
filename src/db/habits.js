39dce7842b0036f2ddc75c00922a3e5b0354a6b9
Yashwanth <yashwanthyashu166@gmail.com>
2023-06-18 18:46:05 +0530

    feat: db setup

---
+++ src/db/habits.js
@@ -0,0 +1,44 @@
+import { v4 as uuid } from "uuid";
+
+export const habits = [
+  {
+    _id: uuid(),
+    name: "Meditate",
+    repeat: "daily",
+    goal: "1 time a day",
+    timeoftheday: "morning",
+    startdate: "today",
+  },
+  {
+    _id: uuid(),
+    name: "Set a To-Do List",
+    repeat: "daily",
+    goal: "1 time a day",
+    timeoftheday: "morning",
+    startdate: "today",
+  },
+  {
+    _id: uuid(),
+    name: "Drink Water",
+    repeat: "daily",
+    goal: "12 time a day",
+    timeoftheday: "anytime",
+    startdate: "today",
+  },
+  {
+    _id: uuid(),
+    name: "Read Books",
+    repeat: "daily",
+    goal: "1 time a day",
+    timeoftheday: "night",
+    startdate: "tommorrow",
+  },
+  {
+    _id: uuid(),
+    name: "Running",
+    repeat: "daily",
+    goal: "1 time a day",
+    timeoftheday: "evening",
+    startdate: "today",
+  },
+];
